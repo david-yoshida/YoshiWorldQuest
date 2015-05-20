5e8960317c4197f5838fe7926d93dd322e770215
David Yoshida <david.yoshida@goradii.com>
2015-05-19 22:47:32 -0400

    First contact

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 ﻿var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    res.writeHead(200, { 'Content-Type': 'text/plain', 'Access-Control-Allow-Origin':'*' });
+
+    res.end('Hello World\n' + Date.now());
+    
+
 }).listen(port);
+
+
+
+
+console.log('Server running at http://127.0.0.1:1337/' + Date.now());
